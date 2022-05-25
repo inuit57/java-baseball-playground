047da60a84dc6b640c8591718976b99581a45714
inuit57 <inuit57@naver.com>
2022-05-25 10:39:43 +0900

    test: contains 테스트

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,6 +1,9 @@
 package study;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -15,6 +18,12 @@ public class SetTest {
         numbers.add(1);
         numbers.add(2);
         numbers.add(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3} )
+    void contains(int number){
+        Assertions.assertTrue(numbers.contains(number));
     }
 
 }
