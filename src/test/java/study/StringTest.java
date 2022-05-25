718ad50c45a0e20dde4e93aabd2fc3c03fd85031
inuit57 <inuit57@naver.com>
2022-05-25 10:08:03 +0900

    test: substring 테스트 작성

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,5 +1,6 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -18,6 +19,13 @@ public class StringTest {
 
         String[] splitOne = "1".split(",");
         assertThat(splitOne).containsExactly("1");
+    }
+
+    @Test
+    void substring(){
+        String given = "(1,2)";
+        String result = given.substring(1, given.length()-1);
+        assertThat(result).isEqualTo("1,2");
     }
 
 
