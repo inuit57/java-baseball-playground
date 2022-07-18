d127d489cf0d88101e04a68c4f2cdc64a8f0e0fb
inuit57 <inuit57@naver.com>
2022-07-18 17:09:16 +0900

    feat : 숫자 3개 생성

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -2,6 +2,10 @@ package study;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringTest {
@@ -9,5 +13,17 @@ public class StringTest {
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    void generateNumbers(){
+        List<Integer> collect = new Random().ints(1, 10)
+                .distinct()
+                .limit(3)
+                .boxed()
+                .sorted()
+                .collect(Collectors.toList());
+
+        System.out.println(collect);
     }
 }
